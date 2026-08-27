d115abf1dbf67e8cc782524ba4797938dba7d30e
Shailan Gosai <sgosai@m-r27lnxr2pr.paloaltonetworks.local>
2026-08-27 09:42:11 -0600

    adding unit test

---
+++ Packs/multiply/Scripts/multiply/multiply_test.py
@@ -0,0 +1,8 @@
+def test_passing():
+    """This test will pass because the assertions are true."""
+    assert 1 + 1 == 2
+    assert "hello".upper() == "HELLO"
+
+def test_failing():
+    """This test will fail intentionally."""
+    assert 1 + 1 == 2
